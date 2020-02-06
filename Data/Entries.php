647d6786475af681965f99e6447587afdb80d712
Adrien Rosi <adrienrosi@gmail.com>
2020-02-06 19:56:14 +0100

    Update entries

--- Data/Entries.php
+++ Data/Entries.php
@@ -2,14 +2,8 @@
 
 namespace AideTravaux\MaPrimeRenov\Data;
 
-use AideTravaux\Core\Entries as BaseEntries;
-use AideTravaux\Anah\Categorie\Entries as AnahCategorieEntries;
+use AideTravaux\Core\Entries as CoreEntries;
 
-abstract class Entries extends BaseEntries
+abstract class Entries extends CoreEntries
 {
-    /**
-     * @property array
-     */
-    const CATEGORIES_ANAH = AnahCategorieEntries::CATEGORIES_ANAH;
-
 }
